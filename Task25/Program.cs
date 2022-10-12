0a4769fc764b0aa0d68d8ebb8ea7e4aaf9914ac9
Sable <bandidos_150@mail.ru>
2022-10-12 23:34:46 +0300

    задача 25

---
+++ Task25/Program.cs
@@ -0,0 +1,20 @@
+﻿int Stepen(int chislo, int pokaz)
+{
+    int result = 0;
+    result = Convert.ToInt32(Math.Pow(chislo, pokaz));
+    return result;
+}
+
+string str;
+int a;
+Console.WriteLine("Введите число:");
+str = Console.ReadLine();
+a = Convert.ToInt32(str);
+string str1;
+int b;
+Console.WriteLine("Введите степень: ");
+str1 = Console.ReadLine();
+b = Convert.ToInt32(str1);
+
+int res1 = Stepen(a, b);
+Console.WriteLine(res1);
