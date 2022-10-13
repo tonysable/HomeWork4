89533df41b360048fd8584b0aec53e7873a1410a
Sable <bandidos_150@mail.ru>
2022-10-13 22:05:39 +0300

    Задача 27

---
+++ Task27/Program.cs
@@ -0,0 +1,18 @@
+﻿int Sum(int a)
+{
+    int result = default;
+    while(a > 0){
+        result += a % 10;
+        a /= 10; 
+    }   
+    return result;
+}
+
+string str;
+int chislo;
+Console.WriteLine("Введите число: ");
+str = Console.ReadLine();
+chislo = Convert.ToInt32(str);
+
+int res1 = Sum(chislo);
+Console.WriteLine(res1);
